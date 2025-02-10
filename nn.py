aca3b3eaf5e355a99493fee257a0a436f1e49889
rodyrahi <45394091+rodyrahi@users.noreply.github.com>
2025-02-10 22:16:31 +0530

    test

--- nn.py
+++ nn.py
@@ -1,46 +1,95 @@
 import sys
 import numpy as np
 import matplotlib
-
-inputs = [1 , 2 , 3 , 2.5]
-# weights1 = [0.2, 0.8, -0.5 , 1.0]
-# weights2 = [0.5, -0.91, 0.26 , -0.5]
-# weights3 = [-0.26, -0.27, 0.17 , 0.87]
+import nnfs
+from nnfs.datasets import spiral_data
+# nnfs.init()
 
 
-weights = [[0.2, 0.8, -0.5 , 1.0],
-           [0.5, -0.91, 0.26 , -0.5],
-           [-0.26, -0.27, 0.17 , 0.87]
-           ]
-
-biases = [2, 3, 0.5]
-
-# weight = [0.2, 0.8, -0.5 , 1.0]
-# bias = 2
-
-output = np.dot(weights, inputs) + biases
-print(output)
+np.random.seed(0)
 
 
 
 
+class Layer_Dense:
+    def __init__(self , n_inputs , n_neurons):
+        self.weights = 0.10*np.random.randn(n_inputs , n_neurons)
+        self.biases = np.zeros((1 , n_neurons))
+    def forward(self, inputs):
+        self.output = np.dot(inputs , self.weights) + self.biases
+
+  
+class Activation_ReLU:
+    def forward(self , inputs):
+        self.output = np.maximum(0 , inputs)
 
 
-# bias1 = 2
-# bias2 = 3
-# bias3 = 0.5
+class Activation_Softmax:
+    def forward(self , inputs):
+        exp_values= np.exp(inputs - np.max(inputs , axis=1 ,keepdims=True))
+        probabilities= exp_values /np.sum(exp_values , axis=1 , keepdims=True)
+        self.output= probabilities
 
-# layer_outputs = []
-# for neuron_weights, neuron_bias in zip(weights, biases):
-#     neuron_output = 0
-#     for n_input, weight in zip(inputs, neuron_weights):
-#         neuron_output += n_input*weight
-#     neuron_output += neuron_bias
-#     layer_outputs.append(neuron_output)
+class Loss:
+    def calculate(self , output , y):
+        sample_losses = self.forward(output , y)
+        data_loss = np.mean(sample_losses)
+        return data_loss
+
+class Loss_CategoricalCrossentropy(Loss):
+    def forward(self , y_pred , y_true):
+        samples = len(y_pred)
+        y_pred_clipped = np.clip(y_pred , 1e-7 , 1-1e-7)
+        if len(y_true.shape) == 1:
+            correct_confidences = y_pred_clipped[range(samples) , y_true]
+        elif len(y_true.shape) == 2:
+            correct_confidences = np.sum(y_pred_clipped*y_true , axis=1)
+
+        negative_log_likelihoods = -np.log(correct_confidences)
+        return negative_log_likelihoods
 
 
-# output = [inputs[0]*weights1[0] + inputs[1]*weights1[1] + inputs[2]*weights1[2] + inputs[3]*weights1[3]  + bias1,
-#           inputs[0]*weights2[0] + inputs[1]*weights2[1] + inputs[2]*weights2[2] + inputs[3]*weights2[3]  + bias2,
-#           inputs[0]*weights3[0] + inputs[1]*weights3[1] + inputs[2]*weights3[2] + inputs[3]*weights3[3]  + bias3
-#           ]
-# print(layer_outputs)
+X,y = spiral_data(samples=100 , classes=3)
+
+
+
+dense1 = Layer_Dense(2,3)
+activation1 = Activation_ReLU()
+
+dense2 = Layer_Dense(3,3)
+activation2 = Activation_Softmax()
+
+dense1.forward(X)
+activation1.forward(dense1.output)
+dense2.forward(activation1.output)
+activation2.forward(dense2.output)
+print(activation2.output[:5])
+
+
+loss_function = Loss_CategoricalCrossentropy()
+loss = loss_function.calculate(activation2.output , y)
+
+print("Loss : " , loss)
+
+# weights = [[0.2, 0.8, -0.5 , 1.0],
+#            [0.5, -0.91, 0.26 , -0.5],
+#            [-0.26, -0.27, 0.17 , 0.87]
+#            ]
+
+# biases = [2, 3, 0.5]
+
+
+# weights2 = [[0.1 , -0.14 , 0.5 ],
+#            [-0.5, 0.12 , -0.33],
+#            [-0.44, 0.73, -0.13]
+#            ]
+
+# biases2 = [-1 , 2  , -0.5]
+
+
+# layer1_output = np.dot( inputs , np.array(weights).T) + biases
+# layer2_output = np.dot( layer1_output , np.array(weights2).T) + biases2
+
+
+# print(layer2_output)
+
